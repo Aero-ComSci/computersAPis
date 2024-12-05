e64bc259c39534f5437b87eb22dec3efe3796892
Mr. Baez <baezlopezolsen@dublinusd.org>
2024-12-04 21:34:59 -0800

    changed endpoint

--- app.py
+++ app.py
@@ -6,20 +6,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    response = requests.post('https://cs-api.pltw.org/baezmatic/increment?id=1')
+    response = requests.post('https://cs-api.pltw.org/computers/increment?id=1')
     return render_template('index.html')
 
 '''def hello():
-    response = requests.get('http://cs-api.pltw.org/baezmatic')
+    response = requests.get('http://cs-api.pltw.org/computers')
     data = response.json()
     key2 = data.get('values',[])
     total_sum = sum(key2)
-    response = requests.post('https://cs-api.pltw.org/baezmatic/increment?id=1')
+    response = requests.post('https://cs-api.pltw.org/computers/increment?id=1')
     return render_template('index.html', content=response.text)'''
    
 @app.route('/api/data')
 def get_data():
-    response = requests.get('http://cs-api.pltw.org/baezmatic')
+    response = requests.get('http://cs-api.pltw.org/computers')
     data = response.json()
     return jsonify(data)
 
@@ -27,7 +27,12 @@ def get_data():
 def button_click():
     
     param = request.args.get('param')
-    response = requests.post(f'https://cs-api.pltw.org/baezmatic?text={param}!')
+    response = requests.post(f'https://cs-api.pltw.org/computers?text={param}!')
+    return response.text
+
+@app.route('/reset')
+def reset():
+    response = requests.post('https://cs-api.pltw.org/computers/reset?password=7285')
     return response.text
 
 if __name__ == '__main__':
